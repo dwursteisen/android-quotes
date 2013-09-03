31c8d4eb65826e5806ea9d8c6ad361b018431851
David Wursteisen <david.wursteisen+git@gmail.com>
2013-09-03 22:35:35 +0200

    creation des boutons

--- Quotes/src/main/java/com/github/dwursteisen/quotes/HomeActivity.java
+++ Quotes/src/main/java/com/github/dwursteisen/quotes/HomeActivity.java
@@ -3,18 +3,18 @@ package com.github.dwursteisen.quotes;
 
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
 import android.widget.TextView;
+
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
-public class HomeActivity
-    extends Activity
-{
+public class HomeActivity extends Activity {
 
     @ViewById
-    TextView hello;
+    Button previous;
 
     @AfterViews
     void afterViews() {
