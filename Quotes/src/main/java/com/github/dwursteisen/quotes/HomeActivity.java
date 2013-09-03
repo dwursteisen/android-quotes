abf0a6657c29604119a2b1375e4327f88a6008e7
David Wursteisen <david.wursteisen+git@gmail.com>
2013-09-03 20:40:27 +0200

    import initial

---
+++ Quotes/src/main/java/com/github/dwursteisen/quotes/HomeActivity.java
@@ -0,0 +1,29 @@
+
+package com.github.dwursteisen.quotes;
+
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
+public class HomeActivity
+    extends Activity
+{
+
+    @ViewById
+    TextView hello;
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater();
+        return true;
+    }
+
+}
